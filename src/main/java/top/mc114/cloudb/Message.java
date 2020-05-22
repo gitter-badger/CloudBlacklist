7b3cd522a16d7176529e0954d816268a7c0a6eb3
ShrBox <shrbox@outlook.com>
2020-05-22 18:05:16 +0800

    0.1.2: 在无权限的群内无法查询云黑名单 将qb命令改为/qb以防止莫名其妙查询黑名单的情况

--- src/main/java/top/mc114/cloudb/Message.java
+++ src/main/java/top/mc114/cloudb/Message.java
@@ -1,9 +1,6 @@
 package top.mc114.cloudb;
 
-import com.sun.tools.javac.util.StringUtils;
-import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.message.GroupMessageEvent;
-import net.mamoe.mirai.message.MessageReceipt;
 import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.MessageUtils;
 import org.jsoup.internal.StringUtil;
@@ -15,12 +12,24 @@ public class Message implements Consumer<GroupMessageEvent> {
     @Override
     public void accept(GroupMessageEvent event) {
         List<Long> list = Main.list;
+        if(event.getGroup().getBotPermission().getLevel()==0||event.getSender().getPermission().getLevel()>0) {
+            return;
+        }
+        Long senderid = event.getSender().getId();
+        if(list.contains(senderid)) {
+            event.getBot().recall(event.getMessage());
+            event.getSender().mute(114514);
+            event.getGroup().sendMessage(MessageUtils.newChain("云黑名单列表中包含")
+                    .plus(new At(event.getSender()))
+                    .plus("["+senderid+"]，已将其处理，原因：https://github.com/ShrBox/CoreBlack-list/blob/master/reasons/"+senderid+".md"));
+            return;
+        }
         String message = event.getMessage().contentToString();
-        if(message.contains("qb")) {
-            String account = message.replace("qb", "").trim();
+        if(message.contains("/qb")) {
+            String account = message.replace("/qb", "").trim();
             if (!StringUtil.isNumeric(account)) {
                 event.getGroup().sendMessage(MessageUtils.newChain(new At(event.getSender()))
-                        .plus("本Bot目前只支持查询QQ号"));
+                        .plus("Core云黑名单目前只支持查询QQ号"));
                 return;
             }
             if (list.contains(Long.parseLong(account))) {
@@ -30,18 +39,6 @@ public class Message implements Consumer<GroupMessageEvent> {
                 event.getGroup().sendMessage(MessageUtils.newChain(new At(event.getSender()))
                         .plus("未在云黑名单列表中找到"));
             }
-        }
-        if(event.getGroup().getBotPermission().getLevel()==0||event.getSender().getPermission().getLevel()>0) {
-            return;
-        }
-        Long senderid = event.getSender().getId();
-        //event.getGroup().sendMessage("接受消息来自："+senderid+"\n列表："+list);
-        if(list.contains(senderid)) {
-            event.getBot().recall(event.getMessage());
-            event.getSender().mute(114514);
-            event.getGroup().sendMessage(MessageUtils.newChain("云黑名单列表中包含")
-                    .plus(new At(event.getSender()))
-                    .plus("["+senderid+"]，已将其处理，原因：https://github.com/ShrBox/CoreBlack-list/blob/master/reasons/"+senderid+".md"));
         }
     }
 }
